68f7843b23d4277f463df115559b4309e80448f3
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-03-19 18:00:14 +0000

    Update models.py

--- patient_data/models.py
+++ patient_data/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from patient_data.model_options.language import LANGUAGE_OPTIONS
 import uuid
 
 
@@ -31,4 +32,34 @@ class Patient(models.Model):
         "UNK": "unknown",
     }
     marital_status = models.CharField(max_length=3, choices=MARITAL_STATUS_OPTIONS)
-    language = models.CharField(max_length=20)
+    language = models.CharField(max_length=5, choices=LANGUAGE_OPTIONS)
+
+
+class Encounter(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    STATUS_OPTIONS = {
+        "planned": "Planned",
+        "in-progress": "In Progress",
+        "on-hold": "On Hold",
+        "discharged": "Discharged",
+        "completed": "Completed",
+        "cancelled": "Cancelled",
+        "discontinued": "Discontinued",
+        "entered-in-error": "Entered in Error",
+        "unknown": "Unknown",
+    }
+    status = models.CharField(max_length=16, choices=STATUS_OPTIONS)
+    CLASS_OPTIONS = {
+        "IMP": "inpatient encounter",
+        "AMB": "ambulatory",
+        "OBSENC": "observation encounter",
+        "EMER": "emergency",
+        "VR": "virtual",
+        "HH": "home health",
+    }
+    encounter_class = models.CharField(max_length=6, choices=CLASS_OPTIONS)
+    coding_system = models.CharField(max_length=80, choices=CLASS_OPTIONS)
+    code = models.CharField(max_length=40)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
