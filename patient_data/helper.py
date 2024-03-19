6cb11515d1760669a82b5aa96a2bbb9af82d2194
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-03-19 18:02:01 +0000

    added config logic to store_fhir_files
    Adding more data to the config will allow users to add different resource types to the DB

--- patient_data/helper.py
+++ patient_data/helper.py
@@ -1,12 +1,15 @@
 from patient_data.models import Patient
 from patient_data.data.patient import get_patient_by_id
+from patient_data.data_structure_config import RESOURCE_CONFIG
 import uuid
 
 
 def store_fhir_files(data: list):
+    processable_resource_types = list(RESOURCE_CONFIG.keys())
     existing_patients = Patient.objects.all()
     existing_patient_ids = list(existing_patients.values_list("id", flat=True))
     bulk_create_patients = []
+    bulk_create_models = {}
     # grab the patient from the entry first and either save the patient or look up the patient (use same uuids)
     # for each entry check that the resource type is in the keys, if not skip
     # if it is, iterate through each of the fields items from the config and use this to grab the data
@@ -22,10 +25,6 @@ def store_fhir_files(data: list):
             )
             main_patient_data = patient_data["resource"]
             patient_id = main_patient_data["id"]
-            """
-            Assuming this would run multiple times in practice
-            I wanted to ensure that we didn't add multiple entries per patient
-            """
             patient_exists = uuid.UUID(patient_id) in existing_patient_ids
             patient = get_patient_by_id(patient_id) if patient_exists else Patient()
             patient.id = patient_id
@@ -38,8 +37,45 @@ def store_fhir_files(data: list):
             patient.city = address_data["city"]
             patient.state = address_data["state"]
             patient.country = address_data["country"]
+            patient.marital_status = main_patient_data["maritalStatus"]["coding"][0][
+                "code"
+            ]
+            patient.language = main_patient_data["communication"][0]["language"][
+                "coding"
+            ][0]["code"]
+            if len(patient.marital_status) > 3:
+                print(patient.marital_status)
             if patient_exists:
                 patient.save()
             else:
                 bulk_create_patients.append(patient)
+            for entry in entries:
+                resource_type = entry["resource"]["resourceType"]
+                if resource_type in processable_resource_types:
+                    resource = RESOURCE_CONFIG[resource_type]
+                    model = resource["model"]
+                    model_instance = model()
+                    model_instance.patient = patient
+                    for field_data in resource["fields"]:
+                        field_value = get_value_from_keys(
+                            field_data["fhir_keys"], entry
+                        )
+                        setattr(
+                            model_instance,
+                            field_data["field_name"],
+                            field_value,
+                        )
+                    if model in bulk_create_models:
+                        bulk_create_models[model].append(model_instance)
+                    else:
+                        bulk_create_models[model] = [model_instance]
     Patient.objects.bulk_create(bulk_create_patients)
+    for model, model_instances in bulk_create_models.items():
+        model.objects.bulk_create(model_instances)
+
+
+def get_value_from_keys(keys, fhir_resource_data):
+    value = fhir_resource_data
+    for key in keys:
+        value = value[key]
+    return value
