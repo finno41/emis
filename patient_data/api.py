1cfde9133bdb6b816a84c17e791cc8c1658af45a
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-03-20 15:50:47 +0000

    added excel export logic

--- patient_data/api.py
+++ patient_data/api.py
@@ -1,4 +1,9 @@
 from patient_data.service import export_data as export_data_service
+from patient_data.excel_logic import create_excel_from_df_data
+from patient_data.dtos.claim import ClaimDTOCollection
+from patient_data.dtos.condition import ConditionDTOCollection
+from patient_data.dtos.encounter import EncounterDTOCollection
+from patient_data.dtos.patients import PatientDTOCollection
 from django.http import HttpResponse
 
 
@@ -8,4 +13,11 @@ def export_data(request):
     claims = export_data["claims"]
     conditions = export_data["conditions"]
     encounters = export_data["encounters"]
-    return HttpResponse("working test")
+    claim_df_data = ClaimDTOCollection(claims).output_df_data()
+    condition_df_data = ConditionDTOCollection(conditions).output_df_data()
+    encounter_df_data = EncounterDTOCollection(encounters).output_df_data()
+    patient_df_data = PatientDTOCollection(patients).output_df_data()
+    excel_response = create_excel_from_df_data(
+        [claim_df_data, condition_df_data, encounter_df_data, patient_df_data]
+    )
+    return excel_response
---
+++ patient_data/excel_logic.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+import io
+import xlsxwriter
+import pandas as pd
+
+
+def create_excel_from_df_data(df_data_list: list):
+    excel_file = io.BytesIO()
+    writer = pd.ExcelWriter(excel_file, engine="xlsxwriter")
+    for df_data in df_data_list:
+        df = df_data["df"]
+        print(df.dtypes)
+        df.to_excel(writer, sheet_name=df_data["title"], index=False)
+    writer.close()
+    excel_file.seek(0)
+    response = HttpResponse(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    response["Content-Disposition"] = 'attachment; filename="patients_upload.xlsx"'
+    response.write(excel_file.read())
+    return response
