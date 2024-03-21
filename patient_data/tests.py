45e9f4aa0073536f5eced0443151e7f3bc2baf7d
Oli Finn <74098129+finno41@users.noreply.github.com>
2024-03-21 14:29:47 +0000

    wrote basic data storage test

--- patient_data/tests.py
+++ patient_data/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from patient_data.models import Patient, Encounter, Condition, Claim
+from patient_data.models import Patient, Encounter, Condition, Claim, RelatedCondition
 from django.db.utils import IntegrityError
-from patient_data.helper import create_patient
+from patient_data.helper import convert_json_files, store_fhir_files
 from django.db import transaction
 from patient_data.test_helper import get_optional_fields_test_data
 from parameterized import parameterized
@@ -11,10 +11,18 @@ from parameterized import parameterized
 
 class HfirTests(TestCase):
     def setUp(self):
-        self.patient = create_patient(
-            "2024-01-01", "New York", "NYC", "US", "male", "A", "da"
-        )
+        self.correct_json_data = {
+            "file": convert_json_files(
+                "patient_data/fhir_data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json"
+            ),
+            "patient_count": 1,
+            "encounter_count": 25,
+            "condition_count": 26,
+            "claim_count": 28,
+            "rel_condition_count": 26,
+        }
 
+    # to test more models this list can be expanded
     optional_fields = [
         {
             "model": Patient,
@@ -60,11 +68,25 @@ class HfirTests(TestCase):
                         model_instance.save()
 
     # test correct data is stored
-    """
-    Unfortunately I ran out of time, but to test the correct data is stored I would mock up some JSON files which I would
-    generate in setUp. I would then grab the objects from the DB and use assertEqual to check
-    them against the expected values
-    """
+    def test_data_storage(self):
+        """
+        Unfortunately I ran out of time, but to test the correct data is stored I would mock up some JSON files which I would
+        generate in setUp. I would then grab the objects from the DB and use assertEqual to check them against the expected values.
+        As a basic test I have initially tested that the correct amount of objects are being stored
+        """
+        json_data = self.correct_json_data
+        store_fhir_files(json_data["file"])
+        patients_stored = Patient.objects.count()
+        encounter_stored = Encounter.objects.count()
+        condition_stored = Condition.objects.count()
+        claim_stored = Claim.objects.count()
+        rel_condition_stored = RelatedCondition.objects.count()
+        self.assertEqual(patients_stored, json_data["patient_count"])
+        self.assertEqual(encounter_stored, json_data["encounter_count"])
+        self.assertEqual(condition_stored, json_data["condition_count"])
+        self.assertEqual(claim_stored, json_data["claim_count"])
+        self.assertEqual(rel_condition_stored, json_data["rel_condition_count"])
+
     # test export is generated
     """
     Initially I would check that an excel file is generated in the API response. I would mock up a request using
